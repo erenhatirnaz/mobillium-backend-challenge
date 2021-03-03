3ec03b480b93e5c810a435e3ebc58a99616d3704
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-03-04 01:37:31 +0300

    Add roles and role_user create table migrations

---
+++ database/migrations/2021_03_03_220945_create_roles_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        // Default roles
+        DB::table('roles')->insert(['name' => "reader"]);
+        DB::table('roles')->insert(['name' => "writer"]);
+        DB::table('roles')->insert(['name' => "moderator"]);
+        DB::table('roles')->insert(['name' => "admin"]);
+
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('role_id')->unsigned()->index();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role_user');
+    }
+}
