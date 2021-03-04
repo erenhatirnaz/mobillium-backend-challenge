ae98eb1f002e5df89ad0a1075915760130192e38
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-03-04 09:05:26 +0300

    Add votes create table migration

---
+++ database/migrations/2021_03_04_060008_create_votes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('article_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
