612a55b359625dca6b42ee85480868efa2f9b22c
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-03-05 12:52:02 +0300

    Add layout component to use in other blade templates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return response("501 Not Implemented!", 501);
+})->name('login');
+
+Route::get('/register', function () {
+    return response("501 Not Implemented!", 501);
+})->name('register');
