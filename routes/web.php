412cb111c4b1fc01a4943c11618fa9e68f52c36d
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-03-05 13:03:35 +0300

    Add HomeController and rename 'welcome' view to 'home'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/login', function () {
     return response("501 Not Implemented!", 501);
