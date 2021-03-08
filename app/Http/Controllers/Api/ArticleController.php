b7f318c996a2d2e81effcc83095547f5ab52f4cd
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-03-08 11:03:08 +0300

    Add Api/ArticleController and map all endpoints for it

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Carbon\Carbon;
+use App\Models\Article;
+use Illuminate\Support\Str;
+use App\Enums\ArticleStatus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class ArticleController extends Controller
+{
+    public function all(Request $request)
+    {
+        if (!$request->user()->can('viewAny', Article::class)) {
+            return response()->json([
+                'code' => 403,
+                'message' => "This action is unauthorized! If you want export your articles, try '/article/export' endpoint.",
+            ])->setStatusCode(403);
+        }
+
+        return response()->json(Article::all()->toArray())->setStatusCode(200);
+    }
+
+    public function export(Request $request)
+    {
+        return response()->json($request->user()->articles()->get()->toArray());
+    }
+
+    public function create(Request $request)
+    {
+        if (!$request->user()->can('create', Article::class)) {
+            return response()->json(['code' => 403, 'message' => "This action is unauthorized!"])->setStatusCode(403);
+        }
+
+        $this->validateArticleRequest($request);
+
+        $publishedAt = Carbon::now();
+        if ($request->input('published_at')) {
+            $publishedAt = Carbon::createFromFormat('Y-m-d H:i', $request->input('published_at'));
+        }
+
+        $article = new Article();
+        $article->user()->associate($request->user());
+        $article->slug = Str::slug($request->input('title')) . "-" . rand(10000, 99999);
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->published_at = $publishedAt;
+        $article->status = ""; // for trigger the Article models' status setter
+        $article->save();
+
+        return response()->json($article->toArray())->setStatusCode(201);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $article = $this->getArticleOrFail($id);
+        $this->userCanUpdate($article, $request->user());
+        $this->validateArticleRequest($request);
+
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->save();
+
+        return response()->json($article->toArray())->setStatusCode(200);
+    }
+
+    public function publish(int $id, Request $request)
+    {
+        $article = $this->getArticleOrFail($id);
+        $this->userCanUpdate($article, $request->user());
+
+        if ($article->status == ArticleStatus::PUBLISHED) {
+            return response()->json([
+                'code' => 422,
+                'message' => "This article is already published!",
+            ])->setStatusCode(422);
+        }
+
+        $article->published_at = Carbon::now();
+        $article->status = ""; // for trigger the Article models' status setter
+        $article->save();
+
+        return response()->json($article->toArray())->setStatusCode(200);
+    }
+
+    public function unpublish(int $id, Request $request)
+    {
+        $article = $this->getArticleOrFail($id);
+        $this->userCanUpdate($article, $request->user());
+
+        if ($article->status !== ArticleStatus::PUBLISHED) {
+            return response()->json([
+                'code' => 422,
+                'message' => "This article is already unpublished!",
+            ])->setStatusCode(422);
+        }
+
+        $article->published_at = null;
+        $article->status = ""; // for trigger the Article models' status setter
+        $article->save();
+
+        return response()->json($article->toArray())->setStatusCode(200);
+    }
+
+    public function delete(int $id, Request $request)
+    {
+        $article = $this->getArticleOrFail($id);
+        if (!$request->user()->can('delete', $article)) {
+            return response()->json([
+                'code' => 403,
+                'message' => "This action is unauthorized!"
+            ])->setStatusCode(403);
+        }
+
+        $article->delete();
+        return response("", 204);
+    }
+
+    private function getArticleOrFail(int $id)
+    {
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json([
+                'code' => 404,
+                'message' => "Not found!"
+            ])->setStatusCode(404);
+        }
+
+        return $article;
+    }
+
+    private function userCanUpdate(Article $article, $user)
+    {
+        if (!$user->can('update', $article)) {
+            return response()->json([
+                'code' => 403,
+                'message' => "This action is unauthorized!"
+            ])->setStatusCode(403);
+        }
+    }
+
+    private function validateArticleRequest($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $errorOneLine = join(" ", $validator->errors()->all());
+            return response()->json([
+                'code' => 422,
+                'message' => $errorOneLine
+            ])->setStatusCode(422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\Auth\LoginController;
 
 /*
@@ -16,4 +17,13 @@ use App\Http\Controllers\Api\Auth\LoginController;
 */
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/article', [ArticleController::class, 'all']);
+    Route::get('/article/export', [ArticleController::class, 'export']);
+    Route::post('/article/create', [ArticleController::class, 'create']);
+    Route::post('/article/update/{id}', [ArticleController::class, 'update']);
+    Route::put('/article/publish/{id}', [ArticleController::class, 'publish']);
+    Route::put('/article/unpublish/{id}', [ArticleController::class, 'unpublish']);
+    Route::delete('/article/delete/{id}', [ArticleController::class, 'delete']);
 });
